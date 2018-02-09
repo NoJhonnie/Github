9768d4aa1393008981d1bd818d4bec54fb25d5d2
NoJhonnie <smgjc123@163.com>
2018-02-09 23:40:56 +0800

    few

--- learnpython/Envs/projects/freshDay/fd_orders/views.py
+++ learnpython/Envs/projects/freshDay/fd_orders/views.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from models import *
 from fd_cart.models import CartInfo
+from fd_user import models
 from django.db import transaction
 from fd_user import user_decorator
+from datetime import datetime
+from decimal import Decimal
 
-
+@user_decorator.login
 def order(request):
     uid = request.session['user_id']
     carts = CartInfo.objects.filter(user_id=uid)
@@ -17,7 +20,43 @@ def order(request):
 @transaction.atomic()
 @user_decorator.login
 def order_handle(request):
-    pass
+    tran_id = transaction.savepoint()
+    cart_ids = request.POST.get('cart_ids')
+    try:
+        order = OrderInfo()
+        nowtime = datetime.now()
+        uid = request.session['user_id']
+        order.oid = '%s%d'%(nowtime.strftime('%Y%m%d%H%M%S'),uid)
+        order.user_id = uid
+        order.odate = nowtime
+        order.ototal = Decimal(request.POST.get('total'))
+        order.save()
+
+        #创建详单对象
+        cart_ids1 = [int(item) for item in cart_ids.split(',')]
+        for id1 in cart_ids1:
+            detail = OrderDetailInfo()
+            detail.order = order
+            cart = CartInfo.objects.get(id=id1)
+            goods = cart.goods
+            if goods.gstock >= cart.count:
+                goods.gstock = cart.goods.gstock - cart.count
+                goods.save()
+                detail.goods_id = goods.id
+                detail.price = goods.gprice
+                detail.count = cart.count
+                detail.save()
+                cart.delete()
+            else:
+                transaction.savepoint_rollback(tran_id)
+                return redirect('/cart/')
+        transaction.savepoint_commit(tran_id)
+    except Exception as e:
+        print '===============%s' %e
+        transaction.savepoint_rollback(tran_id)
+
+    return redirect('/user/order/')
+
 
 @user_decorator.login
 def pay(request,id):
